2b3c3bffa188ab30f13e33b9805e58e7fb244c3d
MicaiahReid <micaiahreid@gmail.com>
2017-11-13 15:56:03 -0500

    added discogs dependency. Start searching for record information

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const pool = require("./lib/db");
 const auth = require("./lib/routes/auth");
+const discogs = require("./lib/routes/discogs");
 
 module.exports = (app) => {
 	app.post("/records/:id", (req, res) => {
@@ -9,22 +9,26 @@ module.exports = (app) => {
 		res.send(recordId);
 	});
 
-	app.post("/records/search/:name", (req, res) => {
-		console.log("Made it to route /records/search/:name");
-		const recordName = req.params.name;
-		console.log("Record name sent was: " + recordName);
-		pool.query("SELECT id, artist_id, name FROM records WHERE name LIKE $1", [recordName], (error, results) => {
-			if(error) {
-				console.log(error.message);
-				res.send(error);
-			}
-			else {
-				console.log("Results found: " + JSON.stringify(results.rows));
-				res.send(results.rows);
-			}
-		});
+	app.post("/search", (req, res) => {
+		// console.log("Made it to route /records/search/:name");
+		// const recordName = req.params.name;
+		// console.log("Record name sent was: " + recordName);
+		// pool.query("SELECT id, artist_id, name FROM records WHERE name LIKE $1", [recordName], (error, results) => {
+		// 	if(error) {
+		// 		console.log(error.message);
+		// 		res.send(error);
+		// 	}
+		// 	else {
+		// 		console.log("Results found: " + JSON.stringify(results.rows));
+		// 		res.send(results.rows);
+		// 	}
+		// });
+		discogs.searchDiscogs(req, res);
 	});
 
+	app.post("/search/:id", (req, res) => {
+
+	});
 	app.post("/login", (req, res) => {
 		auth.authenticateUser(req, res, (valid) => {
 			if(!valid) {
