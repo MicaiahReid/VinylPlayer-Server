927d46182a25672c0ebdb80f9c18ea3cb20b50a4
Jose Medina <jose.medina@aa.soartech.com>
2018-04-18 02:06:47 -0400

    changed network to use full path of model instead of hardcoded path

--- lib/bin/network.py
+++ lib/bin/network.py
@@ -6,6 +6,8 @@ from collections import Counter
 import string
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+current_directory = os.getcwd()
+current_directory = current_directory.replace('\\','/')
 
 def test(x_data):
     characters = sorted(list(set(Counter(string.ascii_letters).keys())))
@@ -27,9 +29,14 @@ def test(x_data):
 
     with tf.Session() as sess:
         # Restore variables from disk.
-        directory = "C:/Users/jose.medina/Documents/UCF/SeniorDesign/Server/VinylPlayer-Server/lib/bin/model"
-        new_saver = tf.train.import_meta_graph("C:/Users/jose.medina/Documents/UCF/SeniorDesign/Server/VinylPlayer-Server/lib/bin/model/model.ckpt.meta")
-        new_saver.restore(sess, tf.train.latest_checkpoint('C:/Users/jose.medina/Documents/UCF/SeniorDesign/Server/VinylPlayer-Server/lib/bin/model/'))
+        print(current_directory)
+        meta_graph_path = current_directory + "/model/model.ckpt.meta"
+        checkpoint_path = current_directory + "/model/"
+        print("Meta Graph Path: " + meta_graph_path)
+        print("Checkpoint Path: " + checkpoint_path)
+
+        new_saver = tf.train.import_meta_graph(meta_graph_path)
+        new_saver.restore(sess, tf.train.latest_checkpoint(checkpoint_path))
         print("Model restored.")
         graph = tf.get_default_graph()
         logits = graph.get_tensor_by_name("logits:0")
