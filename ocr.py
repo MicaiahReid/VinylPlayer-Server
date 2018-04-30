a454ccf1bb6ca25a8ecdeeb441d2568d0f431d1d
Jose Medina <jose.medina@aa.soartech.com>
2018-04-30 19:17:57 -0400

    changed ocr to use original.jpg

--- ocr.py
+++ ocr.py
@@ -28,7 +28,7 @@ def ocr(img, img_height, img_width):
 def test():
     img_width = 1024
     img_height = 512
-    file = current_directory + "/lib/bin/img/lg_ron_2.jpg"
+    file = current_directory + "/lib/bin/img/original.JPG"
     img = cv2.imread(file)
 
     if img is None:
